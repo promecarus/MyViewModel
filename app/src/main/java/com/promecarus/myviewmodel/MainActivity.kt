b340fa7cda3eb8a10e5d91d7d1f15a3528af54bb
promecarus <haikalslipi@gmail.com>
2023-09-08 10:03:38 +0700

    finish

--- app/src/main/java/com/promecarus/myviewmodel/MainActivity.kt
+++ app/src/main/java/com/promecarus/myviewmodel/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.promecarus.myviewmodel
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.promecarus.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         displayResult()
 
