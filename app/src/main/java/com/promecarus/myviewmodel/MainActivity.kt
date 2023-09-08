40fc1d90de3d1ca11743e5bb94c234214e59e6b1
promecarus <haikalslipi@gmail.com>
2023-09-08 09:56:06 +0700

    part 01

--- app/src/main/java/com/promecarus/myviewmodel/MainActivity.kt
+++ app/src/main/java/com/promecarus/myviewmodel/MainActivity.kt
@@ -2,10 +2,48 @@ package com.promecarus.myviewmodel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.promecarus.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
     }
 }
